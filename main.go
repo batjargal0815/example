80671bacefca4bba70ba8ed852e8b33ec5d82708
Batjargal Chuluunkhuu <batjargal.chu@mobicom.mn>
2023-11-23 15:12:38 +0800

    change module name

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
-import "fmt"
-
 type User struct {
 	UserName  string `json:"user_name"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
-}
-
-func main() {
-	user := &User{
-		UserName:  "batja",
-		LastName:  "Batjargal",
-		FirstName: "Chuluunkhuu",
-	}
-
-	fmt.Println("Userinfo:", GetUserInfo(user))
 }
 
 func GetUserInfo(user *User) string {
